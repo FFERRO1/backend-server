9cde2c583c6f154b95cbede2e55dd4c595e2c502
FFERRO1 <65532982+FFERRO1@users.noreply.github.com>
2020-05-28 06:09:23 +0200

    Usuario y token listos

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-
-
-
+// Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Inicializar Variables
 var app = express();
+
+
+// Body parse
+// analizar la aplicación / x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
 // Conexión base de datos
@@ -18,13 +26,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 
 // Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    })
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 
